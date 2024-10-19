d4d897a37a93fde7665e8bb6a02258ad910e1bcc
Cyber <ravshanbekilhamov@gmail.com>
2024-10-19 21:45:22 +0500

    "Done"

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/index2', function () {
+    return view('index2');
+});
+
+
+Route::get('/index3', function () {
+    return view('index3');
+});
+
+
+Route::get('/calendar', function () {
+    return view('pages.calendar');
+});
+
+
+Route::get('/gallery', function () {
+    return view('pages.gallery');
 });
